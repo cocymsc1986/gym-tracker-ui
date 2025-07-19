0860603b0fdc42b8f7109087d14cf1a93ca19c22
Michael Cockcroft <cocymsc@hotmail.com>
2025-07-19 20:59:52 +0100

    feat: add login functionality

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,9 +1,4 @@
-import type { Route } from "./+types/login";
 import { Login } from "@/pages/Login";
-
-export function loader(_args: Route.LoaderArgs) {
-  return {};
-}
 
 export default function Component() {
   return <Login />;
